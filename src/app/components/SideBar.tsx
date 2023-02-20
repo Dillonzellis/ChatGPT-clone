3f318608bc630cca0f6a60b5393e8af0aa07ab44
Dillon Ellis <dillonzellis@gmail.com>
2023-02-20 16:33:11 -0500

    add and delete chats from sidebar and db

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -1,10 +1,22 @@
 "use client";
 
+import { collection, orderBy, query } from "firebase/firestore";
 import { signOut, useSession } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../../../firebase";
+import ChatRow from "./ChatRow";
 import NewChat from "./NewChat";
 
 function SideBar() {
   const { data: session } = useSession();
+
+  const [chats, loading, error] = useCollection(
+    session &&
+      query(
+        collection(db, "users", session.user?.email!, "chats"),
+        orderBy("createdAt", "asc")
+      )
+  );
 
   return (
     <div className="flex h-screen flex-col p-2">
@@ -15,6 +27,9 @@ function SideBar() {
           <div>{/* ModelSelection */}</div>
 
           {/* Map through the ChatsRows */}
+          {chats?.docs.map((chat) => (
+            <ChatRow key={chat.id} id={chat.id} />
+          ))}
         </div>
       </div>
 

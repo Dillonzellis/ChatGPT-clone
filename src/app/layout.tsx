e74067c0f27ae5fb355783855cc8e06e2ba4950c
Dillon Ellis <dillonzellis@gmail.com>
2023-02-20 22:01:57 -0500

    cspell config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,10 +14,6 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
       <body>
         <SessionProvider session={session}>
